f370c0c4f79d9c85c3ca010cdf226819cb47897a
mh <blueskyareahm@gmail.com>
2018-11-06 23:22:18 +0900

    pyspark & spak-data-analytics.

--- pyspark-practice/textStream.py
+++ pyspark-practice/textStream.py
@@ -1,14 +1,32 @@
 from pyspark import SparkContext
 from pyspark.streaming import StreamingContext
 
+import re
+
+def fileName(data):
+    pattern = r"hdfs:"
+    debug = data.toDebugString()
+    repater = re.compile(pattern)
+    matchOB = repater.match(debug)
+    print(matchOB)
+    print(debug)
+
 sc = SparkContext("local[1]", "textStream")
 ssc = StreamingContext(sc, 10)
 
-lines = ssc.textFileStream("/home/xx/tmp") 
-words = lines.flatMap(lambda line: line.split(" ")).filter(lambda x:x)
-pairs = words.map(lambda word: (word, 1))
-wordCounts = pairs.reduceByKey(lambda x, y: x + y)
-wordCounts.pprint()
+lines = ssc.textFileStream("hdfs:///user/")
+
+lines.foreachRDD(fileName)
+
+replaced = lines.map(lambda line: line.replace('[', '').replace(']', '').replace(',',''))
+docids = replaced.map(lambda line: line.split(" ")).filter(lambda x:x)
+docids.count()
+docids.pprint()
+
+#words = lines.flatMap(lambda line: line.split(" ")).filter(lambda x:x)
+#pairs = words.map(lambda word: (word, 1))
+#wordCounts = pairs.reduceByKey(lambda x, y: x + y)
+#wordCounts.pprint()
 
 ssc.start()             
 ssc.awaitTermination() 
