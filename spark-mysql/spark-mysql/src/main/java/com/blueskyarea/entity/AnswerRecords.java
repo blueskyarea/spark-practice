0488409cd529892a0222d7937052410219d8ebc2
blueskyarea <blueskyareahm@gmail.com>
2023-05-21 13:48:26 +0900

    Move spark-mysql

---
+++ spark-mysql/spark-mysql/src/main/java/com/blueskyarea/entity/AnswerRecords.java
@@ -0,0 +1,15 @@
+package com.blueskyarea.entity;
+
+import java.util.Date;
+
+public class AnswerRecords {
+
+	int id;
+	int questionId;
+	int userId;
+	boolean isCorrect;
+	Date createdAt;
+	
+	public AnswerRecords() {
+	}
+}
